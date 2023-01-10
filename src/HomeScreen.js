d86c51b9d4f2ffcbcd4caaae1898a77419251fae
marlowe <marlowepdx@gmail.com>
2023-01-10 13:37:57 -0800

    initial commit, set up firebase

---
+++ src/HomeScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class HomeScreen extends Component {
+  render() {
+    return (
+      <div>HomeScreen</div>
+    )
+  }
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-wtTNyahIYzzqVrDnZBxrGgGHgdQDsU8",
+    authDomain: "netflix-clone-7f0fa.firebaseapp.com",
+    projectId: "netflix-clone-7f0fa",
+    storageBucket: "netflix-clone-7f0fa.appspot.com",
+    messagingSenderId: "354853728422",
+    appId: "1:354853728422:web:3631f2d36862d075a6c467"
+  };
