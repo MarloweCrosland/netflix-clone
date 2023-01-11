7136c8bda8032c6c65cefd6441084d962a7f8aba
marlowe <marlowepdx@gmail.com>
2023-01-10 16:50:34 -0800

    api requests prepared

---
+++ src/Requests.js
@@ -0,0 +1,15 @@
+//TODO: STORE API KEY IN {process.env.API_KEY}
+const API_KEY = "4c8eef3e0b45527ce6a5d602c16e6a47";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
