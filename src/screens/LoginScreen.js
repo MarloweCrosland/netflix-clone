4e783c3f5698bff353977d780dbcd75ab2038fd3
marlowe <marlowepdx@gmail.com>
2023-01-13 11:25:57 -0800

    login Screen conditional render

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import "./LoginScreen.css";
 
 function LoginScreen() {
+    const [signIn, setSignIn] = useState(false);
   return (
     <div className="loginScreen">
       <div className="loginScreen_background">
@@ -12,6 +13,23 @@ function LoginScreen() {
         />
         <button className="loginScreen_button">Sign In</button>
         <div className="loginScreen_gradient" />
+      </div>
+      <div className="loginScreen_body">
+        <>
+          <h1>Unlimited films, TV shows and more.</h1>
+          <h2>Watch anywhere. Cancel at any time.</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </h3>
+          <div className="loginScreen_input">
+              <form>
+                  <input type="email"
+                  placeholder="Email Address"/>
+                  <button className='loginScreen_getStarted'>GET STARTED</button>
+              </form>
+          </div>
+        </>
       </div>
     </div>
   );
