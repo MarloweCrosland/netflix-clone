46238e1ccc4fdd3132197b5db3ca1eb3b37bb731
marlowe <marlowepdx@gmail.com>
2023-01-27 16:17:40 -0800

    login/signup auth functionality and persistant auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //loggedin
+      } else {
+        //logged out
+      }
+    });
+
+    return unsubscribe;
+  },[])
   return (
     <div className="app">
       <Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyD-wtTNyahIYzzqVrDnZBxrGgGHgdQDsU8",
     authDomain: "netflix-clone-7f0fa.firebaseapp.com",
@@ -6,3 +10,11 @@ const firebaseConfig = {
     messagingSenderId: "354853728422",
     appId: "1:354853728422:web:3631f2d36862d075a6c467"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+
+  export { auth };
+  export default db;
